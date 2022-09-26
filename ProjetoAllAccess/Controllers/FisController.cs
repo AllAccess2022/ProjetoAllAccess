d0b8c947a586e863a4bb084b61b9b2034c50cc80
caiqu <caiqu@CAIQUEBRUNO>
2022-09-26 01:17:19 -0300

    Adicionar arquivos de projeto.

---
+++ ProjetoAllAccess/Controllers/FisController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjetoAllAccess.Controllers
+{
+    public class FisController : Controller
+    {
+        public IActionResult Energia()
+        {
+            return View();
+        }
+
+        public IActionResult Trabalho()
+        {
+            return View();
+        }
+
+        public IActionResult Potencia()
+        {
+            return View();
+        }
+
+        public IActionResult Resistores()
+        {
+            return View();
+        }
+
+        public IActionResult Acustica()
+        {
+            return View();
+        }
+
+        public IActionResult FisIndex()
+        {
+            return View();
+        }
+    }
+}
---
+++ ProjetoAllAccess/Controllers/MatController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjetoAllAccess.Controllers
+{
+    public class MatController : Controller
+    {
+        public IActionResult Analisecombinatoria()
+        {
+            return View();
+        }
+
+        public IActionResult Conjunto()
+        {
+            return View();
+        }
+
+        public IActionResult Estatistica()
+        {
+            return View();
+        }
+
+        public IActionResult Financeira()
+        {
+            return View();
+        }
+
+        public IActionResult Geometria()
+        {
+            return View();
+        }
+
+        public IActionResult MatIndex()
+        {
+            return View();
+        }
+    }
+}
