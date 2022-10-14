7b1c438ac18a398e8721d646efa6d411ad726c94
beigola <batatarecheada22@gmail.com>
2022-10-13 21:49:57 -0300

    Teste

--- ProjetoAllAccess/Controllers/FisController.cs
+++ ProjetoAllAccess/Controllers/FisController.cs
@@ -4,27 +4,60 @@ namespace ProjetoAllAccess.Controllers
 {
     public class FisController : Controller
     {
-        public IActionResult Energia()
+        public IActionResult EnergiaIndex()
+        {
+            return View();
+        }
+        public IActionResult Energia1()
+        {
+            return View();
+        }
+        public IActionResult Energia2()
+        {
+            return View();
+        }
+        public IActionResult Energia3()
+        {
+            return View();
+        }
+        public IActionResult Energia4()
+        {
+            return View();
+        }
+        public IActionResult LeiNewtonIndex()
+        {
+            return View();
+        }
+        public IActionResult LeiNewton1()
+        {
+            return View();
+        }
+        public IActionResult LeiNewton2()
+        {
+            return View();
+        }
+        public IActionResult LeiNewton3()
+        {
+            return View();
+        }
+        public IActionResult LeiNewton4()
+        {
+            return View();
+        }
+        public IActionResult TrabalhoIndex()
+        {
+            return View();
+        }
+        public IActionResult Trabalho1()
         {
             return View();
         }
 
-        public IActionResult Trabalho()
+        public IActionResult Trabalho2()
         {
             return View();
         }
-
-        public IActionResult Potencia()
-        {
-            return View();
-        }
-
-        public IActionResult Resistores()
-        {
-            return View();
-        }
-
-        public IActionResult Acustica()
+        public IActionResult Trabalho3()
         {
             return View();
         }
