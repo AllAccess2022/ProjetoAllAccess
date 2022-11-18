6800150a074b5174a0a8d792e729300adefd9f7b
Caíque Bruno Souza Sá <c.bruno@aluno.ifsp.edu.br>
2022-11-18 04:48:35 -0300

    check

--- ProjetoAllAccess/wwwroot/js/site.js
+++ ProjetoAllAccess/wwwroot/js/site.js
@@ -3,8 +3,8 @@
 
 // Write your JavaScript code.
 // Get the modal
-var modal = document.getElementById("myModal");
-var btn = document.getElementById("myBtn");
+var modal = document.getElementById("myModal1");
+var btn = document.getElementById("myBtn1");
 var span = document.getElementsByClassName("close")[0];
 
 
@@ -23,4 +23,30 @@ window.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
     } 
+}
+
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', () => {
+    myInput.focus()
+})
+
+let mybutton = document.getElementById("irTop");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
 }
